246f9c434cac2a5dc9a324d0c922e5a33e2f5268
casey bradford <casey@flatironschool.com>
2020-04-19 21:23:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// add solution here
+var arrayNew = []
+
+function theBeatlesPlay(arrayMusicians, arrayInstruments){
+  for (let i = 0; i < 4; i++){
+  arrayNew.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`);
+  }
+  return arrayNew
+}
+
+function johnLennonFacts(array) {
+  var n = 0
+  var arrayNew2 =[]
+  while (n < array.length){
+  arrayNew2.push(`${array[n]}!!!`)
+  n++;
+}
+return arrayNew2
+}
+
+function iLoveTheBeatles(g){
+  let beatlesArray = []
+  do {beatlesArray.push("I love the Beatles!")}
+  while (g > 5);
+}
